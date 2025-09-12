f81d3978968f5294a64ca2d07196e866edb94165
Kishanlall Kassinathdoss <kishanlall.kassinathdoss@umail.uom.ac.mu>
2025-09-13 00:00:55 +0400

    fix net

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -70,7 +70,7 @@ export default function Home() {
   return (
     <>
       <Helmet>
-        <title>Home | Video Playa</title>
+        <title>Home | Video Playa  | Arnold Pilon</title>
       </Helmet>
 
       <SiteHeader
